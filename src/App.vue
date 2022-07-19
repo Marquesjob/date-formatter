295c47d6bdbcb2ec456a3e65b02836ef043ba29b
Marquesjob <patrickmarques.job@hotmail.com>
2022-07-19 16:16:59 -0300

    - Criando layout

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,99 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script >
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+
+  <div class="infos">
+    <div class="title">Date Formatter</div>
+    <div class="description">
+      <p>Pequeno projeto prático criado para estudo, que tem a finalidade de praticar<br /> o processo de recepção e
+        formatação de datas recebidas de uma <strong>API</strong> Utilizando<strong> Javascript.</strong></p>
+      <p><strong>API</strong> utilizada no projeto:<a href="https://jsonplaceholder.typicode.com/"
+          target="__blank">  JSONPlaceholder — Free Fake REST API.</a></p>
     </div>
-  </header>
+  </div>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="content">
+
+    <div class="container__received">
+      <label for="receveidData"> — Data Recebida — </label>
+      <input id="receveidData" type="text">
+    </div>
+
+    <div class="container__converted">
+      <label for="convertedData"> — Data Convertida — </label>
+      <input id="convertedData" type="text">
+    </div>
+
+  </div>
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style >
+.infos {
+  width: 55%;
+  margin: 0 auto;
+  border-bottom: 1px solid var(--vt-c-green);
+  margin-bottom: 20px;
 }
 
-.logo {
+.description {
+  margin-bottom: 15px;
+}
+
+.title {
+  font-size: 30px;
+  color: var(--vt-c-green);
+  display: flex;
+  justify-content: center;
+}
+
+p {
+  text-align: center;
+}
+
+a {
+  font-style: italic;
+  font-weight: lighter;
+}
+
+strong {
+  font-weight: bold;
+  color: rgb(182, 184, 192);
+}
+
+.content {
+  width: 55%;
+  height: 600px;
+  margin: 0 auto;
+}
+
+input {
+  width: 300px;
+  height: 30px;
   display: block;
-  margin: 0 auto 2rem;
+  margin: 0 auto;
+  border: none;
+  background-color: rgb(65, 65, 65);
+  padding: 5px;
+  color: rgb(190, 190, 190);
+  font-style: italic;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+input:active, input:focus {
+  outline: none;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+label {
+  width: fit-content;
+  height: fit-content;
+  display: block;
+  margin: 0 auto;
+  margin-bottom: 5px;
+  color: rgb(221, 221, 221);
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.container__converted {
+  margin-top: 10px;
 }
 </style>
